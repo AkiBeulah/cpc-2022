46e1ba1978c69224d753a8c4526beecb207c6906
ibidapo07 <fatogunibidapo@gmail.com>
2022-08-12 12:37:19 +0100

    minor change in App.js for pr

--- src/App.js
+++ src/App.js
@@ -5,13 +5,20 @@ import Portrait from "./public/portraits";
 import Pictures from "./public/pictures";
 import Header from "./component/header.component";
 import Footer from "./component/footer.component";
-
+import Voting from "./public/wings";
+import VotingConfirmation from "./public/wingsConfirmation";
+import {useState} from "react";
+import Modal from 'react-modal'
 function App() {
+    const [op, setOp] = useState(false)
+    Modal.setAppElement('#root');
     return (
         <div className='w-screen overflow-x-hidden'>
             <Header/>
             <Routes>
-                <Route path="/" element={<Home/>}/>
+                <Route path="/" element={<Home op={op} setOp={setOp} />}/>
+                <Route path="/wings" element={<Voting />}/>
+                <Route path="/wings_confirmation/:id" element={<VotingConfirmation />}/>
                 
             </Routes>
             <Footer />
