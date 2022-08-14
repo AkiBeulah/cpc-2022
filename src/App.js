b7429cd94c2476ba41bffcbda20939357f49ef4e
Akindele Beulah <akibeulah@gmail.com>
2022-08-14 07:30:02 +0100

    routing fix and folder layout improvement

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import './App.css';
 import Home from "./public/home";
 import Header from "./component/header.component";
 import Footer from "./component/footer.component";
-import Voting from "./public/wings";
-import VotingConfirmation from "./public/wingsConfirmation";
-import {useState} from "react";
+import Voting from "./public/wings_awards/wings";
+import VotingConfirmation from "./public/wings_awards/wingsConfirmation";
+import { useState } from "react";
 import Modal from 'react-modal'
+import LandingPage from "./public/landingPage";
 
 function App() {
     const [op, setOp] = useState(false)
     Modal.setAppElement('#root');
 
     return (
-        <div>
+        <>
             <Header op={op} setOp={setOp} />
             <Routes>
-                <Route path="/" element={<Home op={op} setOp={setOp} />}/>
-                <Route path="/wings" element={<Voting />}/>
-                <Route path="/wings_confirmation/:id" element={<VotingConfirmation />}/>
+                <Route path="/" element={<Home op={op} setOp={setOp} />}>
+                    <Route index element={<LandingPage op={op} setOp={setOp} />} />
+                    <Route path="wings" element={<Voting />} />
+                    <Route path="wings_confirmation/:id" element={<VotingConfirmation />} />
+                </Route>
             </Routes>
             <Footer />
-        </div>
+        </>
     );
 }
 
